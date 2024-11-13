64d3e203ad1fe3abf9be540ade36c2af9dd94915
Lidio Gabriel <lidio.gnascimento@senacsp.edu.br>
2024-11-13 17:30:00 -0300

    corrigindo nome de jogador

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <Funcionario imgURL="https://s2-ge.glbimg.com/giuffh8geETdGlBIWlDItn47gxc=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/e/d/QdtD6IQGqBu7uZFOBhpg/aspas-loud-campeao-cyw.jpg" nomeFunc="Erick Santos" codigoFuncionario="G.O.A.T"/>
         <Funcionario imgURL="https://noticias.maisesports.com.br/wp-content/uploads/2023/03/52775095012_364a6b847d_k.jpg" nomeFunc="Taynah Yukimi" codigoFuncionario="96" />
         <Funcionario imgURL="https://www.pichauarena.com.br/wp-content/uploads/2022/12/Fc-WQmEXgAAUYdu-715x715.jpeg" nomeFunc="Matias Delipetro" codigoFuncionario="69"/>
-        <Funcionario imgURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2awdC37QFdgRiJaqWzlokxFwO5vCxACIgew&s" nomeFunc="Bryan luna" codigoFuncionario="777" />
+        <Funcionario imgURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2awdC37QFdgRiJaqWzlokxFwO5vCxACIgew&s" nomeFunc="Bryan Luna" codigoFuncionario="777" />
       </div>
 
       <footer>
