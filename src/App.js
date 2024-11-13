8d6e1e7e874f3c6eb8216d4d51e20f3a57d84cc0
THIAGO AMARAL SANTOS <thiago.asantos53@senacsp.edu.br>
2024-11-12 22:45:54 -0200

    feito props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './App.scss';
 import Funcionario from './components/funcionario/index.js';
-function App() {
+export default function App() {
+  const [curtida, setCurtida] = useState(0);
+
+  function like(){
+    setCurtida(curtida + 1)
+  }
+
   return (
     <body className='page-LikeDeslike'>
       <header className='header'>
         <h1 className='header-title'>Funcionario do mes</h1>
       </header>
-
+      <div className='props'>
         <Funcionario imgURL="https://s2-ge.glbimg.com/giuffh8geETdGlBIWlDItn47gxc=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/e/d/QdtD6IQGqBu7uZFOBhpg/aspas-loud-campeao-cyw.jpg" nomeFunc="Erick Santos" codigoFuncionario="213"/>
-        <Funcionario imgURL="https://noticias.maisesports.com.br/wp-content/uploads/2023/03/52775095012_364a6b847d_k.jpg" nomeFunc="tayhuhu" codigoFuncionario="123"/>
+        <Funcionario imgURL="https://noticias.maisesports.com.br/wp-content/uploads/2023/03/52775095012_364a6b847d_k.jpg" nomeFunc="tayhuhu" codigoFuncionario="123" like={curtida}/>
+      </div>
+     
+
+      <button onClick={like}>Like</button>
 
     </body>
   );
 }
 
-export default App;
--- src/components/funcionario/index.js
+++ src/components/funcionario/index.js
@@ -1,12 +1,17 @@
 import './index.scss';
 
-export default function Funcionario({imgURL, nomeFunc, codigoFuncionario}) {
+export default function Funcionario({imgURL, nomeFunc, codigoFuncionario, like, deslike}) {
     return(
         <body className='prop-func'>
             <main className='main'>
                 <img src={imgURL}></img>
                 <h2>{nomeFunc}</h2>
                 <p>{codigoFuncionario}</p>
+
+                <div className='curtidas'>
+                    <h1>like: {like}</h1>
+                    <h1>deslike: {deslike}</h1>
+                </div>
             </main>
         </body>
 
