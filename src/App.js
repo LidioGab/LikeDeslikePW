19a19d770fe45f95a4a065ea2246c03d903f6f2e
Lidio Gabriel <lidio.gnascimento@senacsp.edu.br>
2024-11-13 17:09:27 -0300

    project finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { useState } from 'react';
+
 import './App.scss';
 import Funcionario from './components/funcionario/index.js';
 export default function App() {
-  const [curtida, setCurtida] = useState(0);
-
-  function like(){
-    setCurtida(curtida + 1)
-  }
-
   return (
     <body className='page-LikeDeslike'>
       <header className='header'>
         <h1 className='header-title'>Funcionario do mes</h1>
       </header>
       <div className='props'>
-        <Funcionario imgURL="https://s2-ge.glbimg.com/giuffh8geETdGlBIWlDItn47gxc=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/e/d/QdtD6IQGqBu7uZFOBhpg/aspas-loud-campeao-cyw.jpg" nomeFunc="Erick Santos" codigoFuncionario="213"/>
-        <Funcionario imgURL="https://noticias.maisesports.com.br/wp-content/uploads/2023/03/52775095012_364a6b847d_k.jpg" nomeFunc="tayhuhu" codigoFuncionario="123" like={curtida}/>
+        <Funcionario imgURL="https://s2-ge.glbimg.com/giuffh8geETdGlBIWlDItn47gxc=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/e/d/QdtD6IQGqBu7uZFOBhpg/aspas-loud-campeao-cyw.jpg" nomeFunc="Erick Santos" codigoFuncionario="G.O.A.T"/>
+        <Funcionario imgURL="https://noticias.maisesports.com.br/wp-content/uploads/2023/03/52775095012_364a6b847d_k.jpg" nomeFunc="Taynah Yukimi" codigoFuncionario="96" />
+        <Funcionario imgURL="https://www.pichauarena.com.br/wp-content/uploads/2022/12/Fc-WQmEXgAAUYdu-715x715.jpeg" nomeFunc="Matias Delipetro" codigoFuncionario="69"/>
+        <Funcionario imgURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2awdC37QFdgRiJaqWzlokxFwO5vCxACIgew&s" nomeFunc="Bryan luna" codigoFuncionario="777" />
       </div>
-     
 
-      <button onClick={like}>Like</button>
-
+      <footer>
+        <p>CodeCommiters - All Rights Reserved © 2022.</p>
+      </footer>
     </body>
   );
 }
--- src/components/funcionario/index.js
+++ src/components/funcionario/index.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import './index.scss';
+import likeimg from '../../image/like.png';
 
-export default function Funcionario({imgURL, nomeFunc, codigoFuncionario, like, deslike}) {
+export default function Funcionario({imgURL, nomeFunc, codigoFuncionario}) {
+    
+    const [like, setLike] = useState(0);
+    const [deslike, setDeslike] = useState(0);
+    
+    function addLike(){
+        setLike(like + 1);
+    }
+
+    function addDeslike(){
+        setDeslike(deslike + 1);
+    }
     return(
         <body className='prop-func'>
             <main className='main'>
@@ -9,8 +22,14 @@ export default function Funcionario({imgURL, nomeFunc, codigoFuncionario, like, 
                 <p>{codigoFuncionario}</p>
 
                 <div className='curtidas'>
-                    <h1>like: {like}</h1>
-                    <h1>deslike: {deslike}</h1>
+                    <div className='like'>
+                        <h1>like: {like}</h1>
+                        <button onClick={addLike}><img src={likeimg}></img></button>
+                    </div>
+                    <div className='deslike'>
+                        <h1>deslike: {deslike}</h1>
+                        <button onClick={addDeslike}><img src={likeimg}></img></button>
+                    </div>
                 </div>
             </main>
         </body>
